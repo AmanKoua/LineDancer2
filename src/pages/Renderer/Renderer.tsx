bf55184a9878b902643c3013e8b0fe2bdaa0ec2e
Aman <akoua@umich.edu>
2025-02-08 17:18:08 -0500

    added 2nd canvas to renderer, corrected use of getImageData method, and formatted file

--- src/pages/Renderer/Renderer.tsx
+++ src/pages/Renderer/Renderer.tsx
@@ -1,194 +1,215 @@
-import { ICorePageProps, IPoint } from "../../types"
-import { useRef, useEffect} from "react";
+import { ICorePageProps, IPoint } from "../../types";
+import { useRef, useEffect } from "react";
 import { createSeededRNG, sleep } from "./utils";
-import {v4 as uuidv4} from "uuid";
-import { ipcRenderer } from "electron";
 
 import "./Renderer.css";
-import { getSerializedPointsData, registerIpcHandler, serializeAndPersistPointData } from "../rendererIpcService";
+import {
+  getSerializedPointsData,
+  registerIpcHandler,
+  serializeAndPersistPointData,
+} from "../rendererIpcService";
 import { Point } from "./point";
 
 export const Renderer = ({
-    audioFileBuffer,
-    videoFileBuffer,
-    fps,
-    duration,
-    pointCount,
-    width,
-    height,
-    maxDistThresh,
-    rngSeed,    
-    instanceUUID,
-    setAudioFileBuffer,
-    setVideoFileBuffer,
-    setFps,
-    setDuration,
-    setPointCount,
-    setWidth,
-    setHeight,
-    setMaxDistThresh,
-    setInstanceUUID,
-    goToPanel
-}:ICorePageProps) => {
+  audioFileBuffer,
+  videoFileBuffer,
+  fps,
+  duration,
+  pointCount,
+  width,
+  height,
+  maxDistThresh,
+  rngSeed,
+  instanceUUID,
+  setAudioFileBuffer,
+  setVideoFileBuffer,
+  setFps,
+  setDuration,
+  setPointCount,
+  setWidth,
+  setHeight,
+  setMaxDistThresh,
+  setInstanceUUID,
+  goToPanel,
+}: ICorePageProps) => {
+  // TODO : remove temp initialization values!
+  pointCount = 300;
+  width = 500;
+  height = 500;
+  maxDistThresh = 30;
+  fps = 1000; // TODO : revert after testing (to ~30)
+  rngSeed = 1360736; // TODO : take rngSeed from setup component. Store RNGseed in data dir
+  // instanceUUID = uuidv4();
+  instanceUUID = "9893b066-a0b3-4583-a749-9f078b1f9cae"; // TODO : take instanceUUID from setup component
 
-    // TODO : remove temp initialization values!
-    pointCount = 300;
-    width = 500;
-    height = 500;
-    maxDistThresh = 30;
-    fps = 1000; // TODO : revert after testing (to ~30)
-    rngSeed = 1360736; // TODO : take rngSeed from setup component. Store RNGseed in data dir
-    // instanceUUID = uuidv4();
-    instanceUUID = "9893b066-a0b3-4583-a749-9f078b1f9cae"; // TODO : take instanceUUID from setup component
+  let canvasRef = useRef<HTMLCanvasElement>(null);
+  let canvasRefAux = useRef<HTMLCanvasElement>(null);
+  let videoRef = useRef<HTMLVideoElement>(null);
+  let renderInterval: NodeJS.Timeout | undefined = undefined;
+  let points: Point[] = [];
+  let updatePointsCallCount = 0;
 
-    let canvasRef = useRef<HTMLCanvasElement>(null);
-    let videoRef = useRef<HTMLVideoElement>(null);
-    let renderInterval: NodeJS.Timeout | undefined = undefined;
-    let points: Point[] = [];
-    let updatePointsCallCount = 0;
-    
-    const updatePoints = (val: IPoint[]) => {
-        updatePointsCallCount++;
-        points = [];
+  const updatePoints = (val: IPoint[]) => {
+    updatePointsCallCount++;
+    points = [];
 
-        for(let i = 0; i < val.length; i++){
-            points.push(Point.constructFromPointData(val[i]))
-        }
+    for (let i = 0; i < val.length; i++) {
+      points.push(Point.constructFromPointData(val[i]));
+    }
+  };
+
+  registerIpcHandler(points, updatePoints);
+
+  const getPeristedPointData = async (): Promise<boolean> => {
+    let dataRetrievalAttempts = 0;
+
+    while (dataRetrievalAttempts < 3) {
+      getSerializedPointsData(instanceUUID);
+      await sleep(500);
+
+      const didRetrievalAttemptComplete = updatePointsCallCount > 0;
+
+      if (!didRetrievalAttemptComplete) {
+        dataRetrievalAttempts++;
+        continue;
+      }
+
+      return points.length > 0;
     }
 
-    registerIpcHandler(points, updatePoints);
+    return false;
+  };
 
-    const getPeristedPointData = async (): Promise<boolean> => {
-        let dataRetrievalAttempts = 0;
+  const initializePoints = async () => {
+    let didGetPersistedPointsData: boolean = await getPeristedPointData();
 
-        while(dataRetrievalAttempts < 3) {
-            getSerializedPointsData(instanceUUID);
-            await sleep(500);
-    
-            const didRetrievalAttemptComplete = updatePointsCallCount > 0;
-
-            if(!didRetrievalAttemptComplete){
-                dataRetrievalAttempts++;
-                continue;
-            }
-
-            return points.length > 0;
-        }
-
-        return false;
+    if (didGetPersistedPointsData) {
+      return;
     }
 
-    const initializePoints = async () => {
+    const w = width;
+    const h = height;
+    const pc = pointCount;
 
-        let didGetPersistedPointsData: boolean = await getPeristedPointData();
+    var rng = createSeededRNG(rngSeed);
 
-        if(didGetPersistedPointsData){
-            return;
-        }
-
-        const w = width;
-        const h = height;
-        const pc = pointCount;
-
-        var rng = createSeededRNG(rngSeed);
-
-        for(let i = 0; i < pc; i++){
-            points.push(
-                new Point(
-                    Math.floor(rng() * w),
-                    Math.floor(rng() * h),
-                    (rng() * 3) + 1,
-                    (rng() * 3) + 1,
-                )
-            )
-        }
-
-        serializeAndPersistPointData(points, instanceUUID);
-        
+    for (let i = 0; i < pc; i++) {
+      points.push(
+        new Point(
+          Math.floor(rng() * w),
+          Math.floor(rng() * h),
+          rng() * 3 + 1,
+          rng() * 3 + 1
+        )
+      );
     }
 
-    const draw = async () => {
-    
-        if(points.length == 0){
-           await initializePoints();
-        }
+    serializeAndPersistPointData(points, instanceUUID);
+  };
 
-        if(!canvasRef.current){
-            return;
-        }
-
-        const ctx: CanvasRenderingContext2D = canvasRef.current.getContext("2d")!;
-        const w = width;
-        const h = height;
-        const pc = pointCount;
-
-        ctx.clearRect(0,0,w,h);
-        ctx.globalAlpha = 0.4;
-        ctx.drawImage(videoRef.current!, 0, 0); // draws video on top of canvas
-        ctx.globalAlpha = 1;
-
-        /**
-         * TODO : STOPPED HERE!
-         * 
-         * Each pixed will be added as 4 uint8 values to that imageData.data array.
-         * values will be in RGBA order. Get the pixel's brightness.
-         * 
-         */
-        const imageData = ctx.getImageData(0,0,width,height);
-
-
-        for(let i = 0; i < pc; i++){
-            points[i].draw(ctx, pointCount, points, maxDistThresh);
-            points[i].updatePosition(w,h);
-        }
-
+  const draw = async () => {
+    if (points.length == 0) {
+      await initializePoints();
     }
 
-    const startRendering = () => {
-
-        if(!canvasRef.current || renderInterval !== undefined){
-            return;
-        }
-
-        renderInterval = setInterval(()=>{
-            draw();
-        }, fps)
-
+    if (!canvasRef.current || !canvasRefAux.current) {
+      return;
     }
 
-    useEffect(()=>{
+    const ctx: CanvasRenderingContext2D = canvasRef.current.getContext("2d")!;
+    const ctxAux: CanvasRenderingContext2D =
+      canvasRefAux.current.getContext("2d")!;
+    const w = width;
+    const h = height;
+    const pc = pointCount;
 
-        if(renderInterval){
-            return;
-        }
+    ctx.clearRect(0, 0, w, h);
 
-        const temp = setInterval(()=>{
-            startRendering();
+    ctxAux.clearRect(0, 0, w, h);
+    ctxAux.drawImage(videoRef.current!, 0, 0); // draws video on top of canvas
 
-            if(renderInterval){
-                clearInterval(temp);
-            }
-        },100);
+    for (let i = 0; i < pc; i++) {
+      const isPixelInDarkSpace = getIsPixelInDarkSpace(ctxAux, points[i].x, points[i].y);
+      points[i].draw(ctx, pointCount, points, maxDistThresh);
+      points[i].updatePosition(w, h);
+      break; // TODO : Stopped here! Next step is to implement custom bitmap.
+    }
+  };
 
-    },[])
+  const getIsPixelInDarkSpace = (
+    ctxAux: CanvasRenderingContext2D,
+    x: number,
+    y: number
+  ): boolean => {
+    if (x + 1 > width || y + 1 > height) {
+      return false;
+    }
 
-    useEffect(()=>{
+    const imageData = ctxAux.getImageData(x, y, 1, 1);
+    return imageData.data[0] + imageData.data[1] + imageData.data[2] < 30;
+  };
 
-        if(!videoRef.current){
-            return;
-        }
+  const startRendering = () => {
+    if (!canvasRef.current || renderInterval !== undefined) {
+      return;
+    }
 
-        if(!videoFileBuffer){
-            return;
-        }
+    renderInterval = setInterval(() => {
+      draw();
+    }, fps);
+  };
 
-        const blob = new Blob([videoFileBuffer]);
-        videoRef.current.src = URL.createObjectURL(blob);
+  useEffect(() => {
+    if (renderInterval) {
+      return;
+    }
 
-    },[videoRef.current])
+    const temp = setInterval(() => {
+      startRendering();
 
-    return <main className="renderer__main">
-        <video width={width} height={height} ref={videoRef} style={{display:"block"}}controls></video>
-        <canvas width={width} height={height} className="renderer__canvas" id="canvas" ref={canvasRef}></canvas>
+      if (renderInterval) {
+        clearInterval(temp);
+      }
+    }, 100);
+  }, []);
+
+  useEffect(() => {
+    if (!videoRef.current) {
+      return;
+    }
+
+    if (!videoFileBuffer) {
+      return;
+    }
+
+    const blob = new Blob([videoFileBuffer]);
+    videoRef.current.src = URL.createObjectURL(blob);
+  }, [videoRef.current]);
+
+  return (
+    <main className="renderer__main">
+      <video
+        width={width}
+        height={height}
+        ref={videoRef}
+        className="renderer__video"
+        controls
+        muted={true}
+      ></video>
+      <canvas
+        width={width}
+        height={height}
+        className="renderer__canvas__aux"
+        id="canvas-aux"
+        ref={canvasRefAux}
+      ></canvas>
+      <canvas
+        width={width}
+        height={height}
+        className="renderer__canvas"
+        id="canvas"
+        ref={canvasRef}
+      ></canvas>
     </main>
-}
+  );
+};
